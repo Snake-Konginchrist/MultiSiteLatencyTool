3c76c22a3c6fb33127c5a213a5fffe9439fd1864
斯黄 <cyx20110508@163.com>
2024-07-17 09:13:22 +0800

    Fixed the issue where IP queries failed due to the inability to use Google DNS services in Chinese Mainland.

--- src/geoip.js
+++ src/geoip.js
@@ -1,12 +1,40 @@
 const axios = require('axios');
+const dns = require('dns');
+
+async function getIP(domain) {
+    return new Promise((resolve, reject) => {
+        dns.resolve(domain, 'A', (err, addresses) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(addresses[0]);
+            }
+        });
+    });
+}
 
 const IP2LOCATION_API_KEY = '8C6C2D493EC26F1B961CB24B0D3D9CDF'; // 请替换为你的IP2Location.io API密钥
 
 // 从URL获取IP地址并进行地理定位
 async function getGeoLocation(url) {
     try {
-        const ipResponse = await axios.get(`https://dns.google/resolve?name=${url}`);
-        const ip = ipResponse.data.Answer[0].data;
+        // 提取纯域名
+        const domain = url.replace(/^(?:https?:\/\/)?/i, "").split('/')[0];
+        console.log(`Extracted domain: ${domain}`);
+
+        // 请求 DNS 解析
+        // const ipResponse = await axios.get(`https://dns.google/resolve?name=${url}`);
+        // console.log(`DNS response for ${domain}:`, ipResponse.data);
+
+        // 检查 DNS 响应
+        // if (ipResponse.data.Status !== 0 || !ipResponse.data.Answer || ipResponse.data.Answer.length === 0) {
+        //     throw new Error('No valid IP address found in DNS response');
+        // }
+        //
+        // const ip = ipResponse.data.Answer[0].data;
+
+        const ip = await getIP(domain);
+        console.log(`Resolved IP for ${domain}: ${ip}`);
 
         const locationResponse = await axios.get(`https://api.ip2location.io/?key=${IP2LOCATION_API_KEY}&ip=${ip}`);
         const location = `${locationResponse.data.city_name}, 
@@ -18,10 +46,10 @@ async function getGeoLocation(url) {
             location
         };
     } catch (error) {
-        console.error(`获取IP或地理位置信息时出错: ${error}`);
+        console.error(`Error retrieving IP or geographic information: ${error}`);
         return {
-            ip: '未知',
-            location: '未知'
+            ip: 'unknown',
+            location: 'unknown'
         };
     }
 }
